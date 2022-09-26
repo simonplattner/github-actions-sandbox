b2a6d8ad7b7b893a29ebe81e8f095318d601a483
Simon Plattner <5527585+simonplattner@users.noreply.github.com>
2022-09-26 21:04:52 +0200

    Revert "fix(ci): release (deploy to staging for now)"
    This reverts commit 19b8b09c6575d62b9942af03dcd2c1c54782bc2a.

--- src/main/java/com/example/githubactionssandbox/helloworld/HelloWorldController.java
+++ src/main/java/com/example/githubactionssandbox/helloworld/HelloWorldController.java
@@ -1,20 +1,13 @@
 package com.example.githubactionssandbox.helloworld;
 
-import org.springframework.boot.info.BuildProperties;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloWorldController {
 
-  final BuildProperties buildProperties;
-
-  public HelloWorldController(final BuildProperties buildProperties) {
-    this.buildProperties = buildProperties;
-  }
-
   @GetMapping("/")
   public String getHelloWorld() {
-    return "Hello mes dudes! this is version " + buildProperties.getVersion();
+    return "Hello mes dudes!";
   }
 }
